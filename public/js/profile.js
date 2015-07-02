9efd2a01ef50e98ef0b6dcf9027a156ace3f067d
Sridatt Bhamidipati <sbhamidi@ucdavis.edu>
2015-07-01 23:53:18 -0700

    clean ng

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,26 @@
 var profile = angular.module('profile', [])
 .controller('ProfileController', ['$scope', '$http', function($scope, $http){
-
-	$scope.master = {};
-
+	
 	$http({
 		method: 'GET',
 		url: 'api/loginInfo',
 		data: {},
 	}).success(function(data){
-		var name = data.displayName.split(' ');
-		$scope.firstname = name[0];
-		$scope.lastname = name[1];
-	})
+		$scope.user = {
+			firstName: data._json.first_name,
+			lastName: data._json.last_name
+		}
+		$scope.fbid = data.id;
+	});
 
 	$scope.onSave = function(user){
-		$scope.master = angular.copy(user);
-		var userSaveData = $scope.user;
-		console.log(userSaveData);
+		var fbid = $scope.fbid;
 		$http({
-			method: 'GET',
-			url: 'api/loginInfo',
-			data: {},
-		}).success(function(profileData){
-			console.log(profileData.id);
-			$http({
-				method: 'PUT',
-				url: '/api/saveInfo/' + profileData.id,
-				data: userSaveData
-			}).success(function(data){
-				console.log('youve saved');
-				console.log(data);
-			})
+			method: 'PUT',
+			url: '/api/saveInfo/' + fbid,
+			data: userSaveData
+		}).success(function(data){
+			console.log(data);
 		})
 	};
 }]);
